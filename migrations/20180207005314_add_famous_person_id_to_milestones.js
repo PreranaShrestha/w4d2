1c29a9e32d458c557ede9145438c55d5d8cc8f39
PreranaShrestha <prerana.sh@gmail.com>
2018-02-07 02:29:43 +0000

    Add forigen key in milestone

--- migrations/20180207005314_add_famous_person_id_to_milestones.js
+++ migrations/20180207005314_add_famous_person_id_to_milestones.js
@@ -4,12 +4,16 @@ exports.up = function(knex, Promise) {
   return Promise.all([
     knex.schema.table('milestones', function(t){
       t.integer('famous_person_id');
+      t.foreign('famous_person_id').references('famous_people.id');
     })
   ])
 };
 
 exports.down = function(knex, Promise) {
   return Promise.all([
-    knex.schema.dropTable('milestones')
+    knex.schema.table('milestones', function(t){
+      t.dropForeign('famous_person_id')
+      t.dropColumn('famous_person_id')
+  })
   ])
 };
